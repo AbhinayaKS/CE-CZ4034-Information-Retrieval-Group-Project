5cfc28640c76e0aaf2a8fa3976e227b0175fd659
Shi_J_C <76093941+Shi-J-C@users.noreply.github.com>
2023-03-26 14:55:46 +0800

    Add files via upload

---
+++ Application/src/App.js
@@ -0,0 +1,185 @@
+import React, { useState,useEffect } from 'react';
+import axios from 'axios';
+import DatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css';
+import './App.css';
+
+function App() {
+
+  const genres_url = 'http://localhost:8983/solr/movies/select?q=*:*&facet=true&facet.field=Genre_s_&facet.limit=-1&facet.mincount=1'
+
+  const [query, setQuery] = useState('');
+  const [condition, setCondition] = useState([]);
+  const [results, setResults] = useState([]);
+  const [startDate, setStartDate] = useState(null);
+  const [endDate, setEndDate] = useState(null);
+
+  const [currentPage, setCurrentPage] = useState(1);
+  const itemsPerPage = 8;
+
+  const movieGenres = ["Action","Adventure","Animation","Crime","Thriller","Fantasy","Science Fiction","Comedy","Drama","Family","Mystery","Horror","Romance","History","War"]
+  const production_Company = ["A","B","C"]
+
+  const dataResult = [{"title":"Movie A","genres":"A,B,C,D,E"},
+                      {"title":"Movie A1","genres":"A,B,C,D,E"},
+                      {"title":"Movie A2","genres":"A,B,C,D,E"},
+                      {"title":"Movie A3","genres":"A,B,C,D,E"},
+                      {"title":"Movie A","genres":"A,B,C,D,E"},
+                      {"title":"Movie A","genres":"A,B,C,D,E"},
+                      {"title":"Movie A","genres":"A,B,C,D,E"},
+                      {"title":"Movie A4","genres":"A,B,C,D,E"},
+                      {"title":"Movie A","genres":"A,B,C,D,E"},
+                      {"title":"Movie A","genres":"A,B,C,D,E"},
+                      {"title":"Movie A","genres":"A,B,C,D,E"},
+                      {"title":"Movie A","genres":"A,B,C,D,E"}]
+  movieGenres.sort((a, b) => a.localeCompare(b));
+
+   // State variable for filtered results
+   const [filteredResults, setFilteredResults] = useState(dataResult);
+
+   // Filter results based on query state variable
+   useEffect(() => {
+     if (condition) {
+      console.log(condition)
+       const filteredData = dataResult.filter((item) => {
+         return item.title.toLowerCase().includes(condition);
+       });
+       setFilteredResults(filteredData);
+     } else {
+       setFilteredResults(dataResult);
+     }
+   }, [condition]);
+
+
+   // Calculate the number of pages
+  const numberOfPages = Math.ceil(filteredResults.length / itemsPerPage);
+
+  // Get the current page's data
+  const getPageData = () => {
+    const startIndex = (currentPage - 1) * itemsPerPage;
+    const endIndex = startIndex + itemsPerPage;
+    return filteredResults.slice(startIndex, endIndex);
+  }
+
+  const handleInputChange = (event) => {
+    setQuery(event.target.value);
+  };
+
+  const handleDateChange = (dates) => {
+    const [start, end] = dates;
+    setStartDate(start);
+    setEndDate(end);
+  };
+
+  const handleConditionChange = (e) => {
+    const genre = e.target.value;
+    const isChecked = e.target.checked;
+    if (isChecked) {
+      setCondition([...condition, genre]);
+    } else {
+      setCondition(condition.filter((g) => g !== genre));
+    }
+  }
+
+  const submit = (e) => {
+    e.preventDefault();
+    try {
+      const response = axios.post('/url', { query});
+      setResults(response.data);
+    } catch (error) {
+      console.log(query)
+      console.error(error);
+    }
+  }
+
+  return (
+    <div className="container">
+      <div className='header'>
+        <h1>IMDB DATA</h1>
+      </div>
+      <div className='section1'>
+      <h2>IMDb Search</h2>
+      <div>
+        <form onSubmit={submit}>
+          <div>
+            <div>
+              <input type="text" name="search_text" id="search_text" className="form-control" placeholder="Enter your query..." value={query} onChange={handleInputChange}/>
+            </div>
+            <div>
+            </div>
+          </div>
+        </form>
+      </div>
+      <div>
+        <h4>Movie genres</h4>
+        <div className='genres_container'>
+          {movieGenres.map((genre) => {
+          return (
+            <div class="form-check" key={genre}>
+            <input class="form-check-input" type="checkbox"  value={genre} id={genre} onChange={(e) => handleConditionChange(e)}/>
+            <label class="form-check-label" htmlFor={genre}>
+              {genre}
+            </label>
+          </div>
+          )
+          })}
+        </div>
+      </div>
+      <div>
+        <h4>Movie Date</h4>
+        <div>
+          <DatePicker
+            selected={startDate ? [startDate, endDate] : null}
+            onChange={handleDateChange}
+            startDate={startDate}
+            endDate={endDate}
+            selectsRange
+            inline
+          />
+        </div>
+      </div>
+      <div>
+        <h4>Production Company</h4>
+        {production_Company.map((element) => {
+        return (
+          <div class="form-check">
+          <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault"/>
+          <label class="form-check-label" for="flexCheckDefault">
+            {element}
+          </label>
+        </div>
+        )
+        })}
+      </div>
+
+      </div>
+      <div className='section2'>
+        <div className="row">
+          {getPageData().map((item, index) => (
+            <div className="box" key={index}>
+              <h1>{item.title}</h1>
+              <h1>{item.genres}</h1>
+            </div>
+          ))}
+        </div>
+        <nav>
+          <ul className="pagination">
+            <li className={`page-item${currentPage === 1 ? ' disabled' : ''}`}>
+              <button className="page-link" onClick={() => setCurrentPage(currentPage - 1)}>Previous</button>
+            </li>
+            {Array(numberOfPages).fill().map((_, i) => (
+              <li className={`page-item${i + 1 === currentPage ? ' active' : ''}`} key={i}>
+                <button className="page-link" onClick={() => setCurrentPage(i + 1)}>{i + 1}</button>
+              </li>
+            ))}
+            <li className={`page-item${currentPage === numberOfPages ? ' disabled' : ''}`}>
+              <button className="page-link" onClick={() => setCurrentPage(currentPage + 1)}>Next</button>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </div>
+  );
+}
+
+export default App;
